86ea6f77e37de9e7427c5b52c362245682b0d976
Marshall Brekka <marshallsmedia@gmail.com>
2012-04-08 16:51:47 -0700

    homepage format

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ if($_POST['numbers'] != null) {
 <form method="post">
     <label>Phone Numbers (each one on a new line, it works to just copy a column from google docs)</label><br/>
     <textarea name="numbers"></textarea><br/>
-    <label>Message (160 characters) Include bit.ly/asuc2012 in the message for a trackable link to the asuc elections page</label><br/>
+    <label>Message (160 characters) Include http://bit.ly/asuc2012 in the message for a trackable link to the asuc elections page</label><br/>
     <textarea name="message"></textarea><br/>
     <input type="submit"/>
     
