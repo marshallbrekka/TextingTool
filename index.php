d1e89a13379f412c91b5aab4e1226526af8243d4
Marshall Brekka <marshallsmedia@gmail.com>
2012-04-08 16:41:32 -0700

    added mail

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <pre>
 <?php
 require 'php/NumberParser.php';
+require 'php/Mail.php';
 
 $p = new NumberParser();
 if($_POST['numbers'] != null) {
-    print_r($p->buildEmails($_POST['numbers']));
+    $emails = $p->buildEmails($_POST['numbers']);
+    $from = "KleinLieuForASUCSenate@gmail.com";
+    $mail = new Mail();
+    foreach($emails as $email) {
+        $mail->sendMail($from, $email, $_REQUEST['message']);
+    }
+    
 }
 
 
@@ -13,7 +20,11 @@ if($_POST['numbers'] != null) {
 ?>
 </pre>
 <form method="post">
+    <label>Phone Numbers (each one on a new line, it works to just copy a column from google docs)</label>
     <textarea name="numbers"></textarea>
+    <label>Message (160 characters)</label>
+    <textarea name="message"></textarea>
     <input type="submit"/>
     
 </form>
+
